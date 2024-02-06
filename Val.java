9db7527ed4c9d7614ee80501aea1138b521f4e9a
glenndsilva1 <glenndsilva@rediffmail.com>
2024-02-06 20:32:02 +0530

    Val.java

---
+++ Val.java
@@ -0,0 +1,19 @@
+package withdecorator;
+
+import java.util.ArrayList;
+
+public class Val implements Validity {
+	
+	public Val(ArrayList<String> k) {
+		
+	}
+	
+	@Override
+	public Val validations(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
